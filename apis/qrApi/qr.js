886b81f670fc431595e545757b825689b41b20f0
Sudhanshu <sudhanshu02512@gmail.com>
2025-03-30 23:25:51 +0530

    Added QR Post Method

--- apis/qrApi/qr.js
+++ apis/qrApi/qr.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const qr = require('qr-image');
 
-/** QR Router */
+/** QR GET Router */
 router.get('/',async (req,res)=>{
     const text = req.body?.text;
     if(text){
@@ -12,7 +12,18 @@ router.get('/',async (req,res)=>{
     else{
         res.json({"error":"Please provide text"});
     }
-    
+});
+
+/** QR POST Router */
+router.post('/',async (req,res)=>{
+    const text = req.body?.text;
+    if(text){
+        let svg_string = qr.imageSync(text, { type: 'svg' });  
+        res.send(svg_string);
+    }
+    else{
+        res.json({"error":"Please provide text"});
+    }
 });
 
 module.exports = router;
