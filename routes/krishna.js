21aa6f41c2bd3047f8d8a6390c55e8dbcc82a056
Sudhanshu <sudhanshu02512@gmail.com>
2025-08-09 17:21:17 +0530

    Enhanced response

--- routes/krishna.js
+++ routes/krishna.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+//Read package.json
+
+const {name,version} = require('../package.json');
+
 router.get('/',(req,res)=>{
-    res.send('Krishna');
+    let response ={
+        status: 'success',
+        service: name,
+        version: version,
+        uptime: `${process.uptime().toFixed(2)} seconds`,
+        timestamp: new Date().toISOString(),
+        message: 'Service is running smoothly 🚀',
+        idol:'⭕‼️⭕',
+        support:"In case of any issue please contact sudhanshu02512@gmail.com"
+    }
+    res.send(response);
 })
 
 module.exports = router;
