366f660b89d76e9f9fa2df03828a5a8b49a2cc95
Sudhanshu <sudhanshu02512@gmail.com>
2025-08-11 00:05:14 +0530

    Init API Switches & Security Patch 11082025v1

--- apis/geminiApi/gemini.js
+++ apis/geminiApi/gemini.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-require('dotenv').config();
+require("dotenv").config();
 
 /**Gemini Configuration */
 const { GoogleGenerativeAI } = require("@google/generative-ai");
@@ -8,10 +8,31 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 /** Gemini Router */
-router.get('/',async (req,res)=>{
+router.get("/", async (req, res) => {
+  if (process.env.GEMINI_API_SWTICH == "TRUE") {
     const prompt = req.body?.prompt;
-    const result = prompt ? await model.generateContent(prompt) : {"error":"Please provide prompt in the body"};
-    res.json(result);
+    if(prompt){
+        const result = await model.generateContent(prompt);
+        res.status(200).json(result);
+    }
+    else{
+        const response = { error: "Please provide prompt in the body" };
+        res.status(400).json(response);
+    }
+  } else {
+    const response = {
+      status: 503,
+      message: "Gemini Web Service Unavailable",
+      error: {
+        code: "SERVER_DOWN",
+        description:
+          "The server is currently down for maintenance or experiencing issues.",
+        retry_after_seconds: 120,
+      },
+      timestamp: new Date().toISOString(),
+    };
+    res.status(503).json(response);
+  }
 });
 
 module.exports = router;
--- apis/staticJsonApi/staticJson.js
+++ apis/staticJsonApi/staticJson.js
@@ -1,56 +1,73 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+require("dotenv").config();
 
-router.get('/',(req,res)=>{
+router.get("/", (req, res) => {
+  if (process.env.STATIC_JSON_API_SWITCH == "TRUE") {
     const response = {
-    "status": 200,
-    "message": "OK",
-    "response": {
-        "book_uuid": "19aab921-25a5-4beb-a59e-ac72320061d6",
-        "cash_flow_features": {
-        "analysis_period": {
-            "start_date": "2025-01-01",
-            "end_date": "2025-06-30"
-        },
-        "cash_flow_items": [
+      status: 200,
+      message: "OK",
+      response: {
+        book_uuid: "19aab921-25a5-4beb-a59e-ac72320061d6",
+        cash_flow_features: {
+          analysis_period: {
+            start_date: "2025-01-01",
+            end_date: "2025-06-30",
+          },
+          cash_flow_items: [
             {
-            "category": "Income",
-            "total_amount": 27000.00,
-            "average_monthly_amount": 4500.00,
-            "transaction_count": 6
+              category: "Income",
+              total_amount: 27000.0,
+              average_monthly_amount: 4500.0,
+              transaction_count: 6,
             },
             {
-            "category": "Housing",
-            "total_amount": -9000.00,
-            "average_monthly_amount": -1500.00,
-            "transaction_count": 6
+              category: "Housing",
+              total_amount: -9000.0,
+              average_monthly_amount: -1500.0,
+              transaction_count: 6,
             },
             {
-            "category": "Debt Payments",
-            "total_amount": -3000.00,
-            "average_monthly_amount": -500.00,
-            "transaction_count": 6
-            }
-        ]
+              category: "Debt Payments",
+              total_amount: -3000.0,
+              average_monthly_amount: -500.0,
+              transaction_count: 6,
+            },
+          ],
         },
-        "review_required": true,
-        "review_reasons": [
-        "Authenticity score below threshold", 
-        "Anomaly in transactions detected"
+        review_required: true,
+        review_reasons: [
+          "Authenticity score below threshold",
+          "Anomaly in transactions detected",
         ],
-        "authenticity_score": {
-        "score": 45,
-        "reason_codes": [
+        authenticity_score: {
+          score: 45,
+          reason_codes: [
             {
-            "code": "120-M",
-            "confidence": "MEDIUM",
-            "description": "Possible balance tampering"
-            }
-        ]
-        }
-    }
-    }
-    res.send(response);
+              code: "120-M",
+              confidence: "MEDIUM",
+              description: "Possible balance tampering",
+            },
+          ],
+        },
+      },
+    };
+    res.status(200).send(response);
+  }
+  else{
+    const response = {
+      status: 503,
+      message: "Static JSON Web Service Unavailable",
+      error: {
+        code: "SERVER_DOWN",
+        description:
+          "The server is currently down for maintenance or experiencing issues.",
+        retry_after_seconds: 120,
+      },
+      timestamp: new Date().toISOString(),
+    };
+    res.status(503).json(response);
+  }
 });
 
 module.exports = router;
